98106e26412237064d30405020219ae727db9fdf
Walter Doekes <walter+github@wjd.nu>
2015-09-07 23:15:32 +0200

    Simple curlies/parentheses tests for applications.

--- asterisklint/application.py
+++ asterisklint/application.py
@@ -1,4 +1,10 @@
+# vim: set ts=8 sw=4 sts=4 et ai:
+from .defines import WarningDef
 
+
+if 'we_dont_want_two_linefeeds_between_classdefs':  # for flake8
+    class W_APP_BALANCE(WarningDef):
+        message = 'looks like unbalanced parenthesis/quotes/curlies'
 
 
 class App(object):
@@ -23,3 +29,52 @@ class App(object):
         # FIXME: parse app
         self.raw = app
         self.where = where
+
+        # Attempt to parse the app.
+        self.parse()
+
+    def parse(self):
+        # Fetch the Application. If we don't know specifically, we can
+        # call the parse_simple on it.
+        # FIXME: try to find a proper handler first (Set?)
+        try:
+            self.parse_simple(self.raw)
+        except ValueError:
+            W_APP_BALANCE(self.where)
+
+    @staticmethod
+    def parse_simple(app):
+        # TODO: we don't check backslash escapes here
+        # TODO: write proper tests for this?
+        arr = ['X']
+        for char in app:
+            if char == '"':
+                if arr[-1] == '"':
+                    arr.pop()
+                elif arr[-1] == "'":
+                    pass
+                else:
+                    arr.append('"')
+            elif char == "'":
+                if arr[-1] == "'":
+                    arr.pop()
+                elif arr[-1] == '"':
+                    pass
+                else:
+                    arr.append("'")
+            elif char in '({[':
+                if arr[-1] in '\'"':
+                    pass
+                else:
+                    arr.append(char)
+            elif char in ')}]':
+                left = '({['[')}]'.index(char)]
+                if arr[-1] in '\'"':
+                    pass
+                else:
+                    if arr[-1] == left:
+                        arr.pop()
+                    else:
+                        raise ValueError(''.join(arr[1:]))
+        if arr != ['X']:
+            raise ValueError(''.join(arr[1:]))
