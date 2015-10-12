d5ec57bb18db0ab35688d6a21d8020877d6142b7
Walter Doekes <walter+github@wjd.nu>
2015-10-12 16:38:40 +0200

    Add AppBase separate_args.

--- asterisklint/app/base.py
+++ asterisklint/app/base.py
@@ -22,11 +22,11 @@ class AppBase(object):
             W_APP_BALANCE(where, data=data)
 
     @staticmethod
-    def check_balance(app):
+    def check_balance(data):
         # TODO: we don't check backslash escapes here, should we?
         # TODO: write proper tests for this?
         arr = ['X']
-        for char in app:
+        for char in data:
             if char == '"':
                 if arr[-1] == '"':
                     arr.pop()
@@ -57,3 +57,57 @@ class AppBase(object):
                         raise ValueError(''.join(arr[1:]))
         if arr != ['X']:
             raise ValueError(''.join(arr[1:]))
+
+    @staticmethod
+    def separate_args(data, delim=',', remove_quotes_backslashes=True):
+        """
+        #define AST_STANDARD_APP_ARGS(...) => separate_args(',', True)
+        #define AST_STANDARD_RAW_ARGS(...) => separate_args(',', False)
+
+        SOURCE: main/app.c -- __ast_app_separate_args()
+
+        TODO: we should separate args using a more sensible approach as
+        well, so we can warn on inconsistencies.
+        """
+        brackets = 0
+        parens = 0
+        quotes = False
+        skipnext = False
+
+        ret = [[]]
+        start = 0
+        for i, char in enumerate(data):
+            if skipnext:
+                skipnext = False
+            elif char == '[':
+                brackets += 1
+            elif char == ']':
+                if brackets:
+                    brackets -= 1
+            elif char == '(':
+                parens += 1
+            elif char == ')':
+                if parens:
+                    parens -= 1
+            elif char == '"' and delim != '"':
+                quotes = not quotes
+                if remove_quotes_backslashes:
+                    ret[-1].append(data[start:i])
+                    start = i + 1
+            elif char == '\\':
+                if remove_quotes_backslashes:
+                    ret[-1].append(data[start:i])
+                    start = i + 1
+                skipnext = True
+            elif char == delim and not (brackets or parens or quotes):
+                ret[-1].append(data[start:i])
+                start = i + 1
+                ret.append([])  # start on next arg
+
+        # Append leftover args.
+        ret[-1].append(data[start:])
+
+        # Squash args.
+        ret = [''.join(i) for i in ret]
+
+        return ret
---
+++ tests/application/test_base.py
@@ -0,0 +1,72 @@
+from asterisklint.alinttest import ALintTestCase
+from asterisklint.app.base import AppBase
+
+
+class AppBaseSeparateArgsTest(ALintTestCase):
+    def test_abc_def_ghi(self):
+        self.assertEqual(
+            AppBase.separate_args('abc,def,ghi'),
+            ['abc', 'def', 'ghi'])
+
+    def test_quoted_abc(self):
+        self.assertEqual(
+            AppBase.separate_args('"abc"'),
+            ['abc'])
+
+    def test_quoted_abc_def(self):
+        self.assertEqual(
+            AppBase.separate_args('"abc",def'),
+            ['abc', 'def'])
+
+    def test_quoted_abc_def_ghi(self):
+        self.assertEqual(
+            AppBase.separate_args('abc,"def","ghi"'),
+            ['abc', 'def', 'ghi'])
+
+    def test_other_delimiter(self):
+        self.assertEqual(
+            AppBase.separate_args('a,b,c|"d,e,f"|"ghi"', '|'),
+            ['a,b,c', 'd,e,f', 'ghi'])
+
+    def test_respect_quotes(self):
+        self.assertEqual(
+            AppBase.separate_args('"a,b,c","d",e,f'),
+            ['a,b,c', 'd', 'e', 'f'])
+
+    def test_respect_quotes_raw(self):
+        self.assertEqual(
+            AppBase.separate_args(
+                '"a,b,c","d",e,f', remove_quotes_backslashes=False),
+            ['"a,b,c"', '"d"', 'e', 'f'])
+
+    def test_respect_backslashes(self):
+        self.assertEqual(
+            AppBase.separate_args('"abc\\",\\"def"'),
+            ['abc","def'])
+
+    def test_respect_backslashes_raw(self):
+        self.assertEqual(
+            AppBase.separate_args(
+                '"abc\\",\\"def"', remove_quotes_backslashes=False),
+            ['"abc\\",\\"def"'])
+
+    def test_respect_brackets(self):
+        self.assertEqual(
+            AppBase.separate_args('abc,def[g[h[i],j],kl],mno'),
+            ['abc', 'def[g[h[i],j],kl]', 'mno'])
+
+    def test_respect_parens(self):
+        self.assertEqual(
+            AppBase.separate_args('abc,def(g(h(i),j),kl),mno'),
+            ['abc', 'def(g(h(i),j),kl)', 'mno'])
+
+    def test_respect_no_negative_close(self):
+        self.assertEqual(
+            AppBase.separate_args('abc)))(((,))),def'),
+            ['abc)))(((,)))', 'def'])
+
+    def test_respect_brackets_parens(self):
+        # TODO: crazy bracket/parens combinations, we should warn here..
+        self.assertEqual(
+            AppBase.separate_args('abc,def[g(h]i(,j],k)),l],m([),]n)o'),
+            ['abc', 'def[g(h]i(,j],k))', 'l]', 'm([),]n)o'])
